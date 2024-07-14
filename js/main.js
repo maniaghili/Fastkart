2494fe5f7701724c9ef271880793e58c9ef7385e
maniaghili <aligholimani@gmail.com>
2024-07-14 10:51:03 +0330

    Update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 console.log('salaam');
 
-function man(){
-    console.log(arguments[1]||[0]);
+let topsave = document.querySelector('.top_save_f') 
+
+let second = 59
+let menete = 59
+let huor = 10 
+
+let timer = setInterval(function(){
+topsave.innerHTML = `Expires in:${huor}:${menete}:${second}`
+second--
+
+if(second == -1){
+    menete--
+    second =59
+} 
+
+if(menete == -1){
+    huor--
+    menete = 59
 }
 
-man(2,3);
-
+}, 1000);
 
 
